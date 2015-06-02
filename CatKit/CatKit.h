e6b67e72db7daa4bfc87f79280630213dd8993b4
József Vesza <jozsef.vesza@outlook.com>
2015-06-02 16:13:45 +0200

    cleanup

--- CatKit/CatKit.h
+++ CatKit/CatKit.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double CatKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char CatKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CatKit/PublicHeader.h>
-
-
