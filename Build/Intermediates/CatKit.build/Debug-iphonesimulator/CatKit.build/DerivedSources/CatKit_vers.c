3ce88e676f08f51c6d3ef3580eb4233b5526f4c6
József Vesza <jozsef.vesza@outlook.com>
2015-06-02 14:42:40 +0200

    New gitignore

--- Build/Intermediates/CatKit.build/Debug-iphonesimulator/CatKit.build/DerivedSources/CatKit_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char CatKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CatKit  PROJECT:CatKit-1" "\n"; const double CatKitVersionNumber __attribute__ ((used)) = (double)1.;
